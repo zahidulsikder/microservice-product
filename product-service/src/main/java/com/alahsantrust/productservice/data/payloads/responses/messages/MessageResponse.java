b8e84e0a3cd48c84426c5472c0e901c1dbec820f
zahidulsikder <mdzahidulsikder@gmail.com>
2023-12-25 01:19:52 +0600

    youtube link

--- product-service/src/main/java/com/alahsantrust/productservice/data/payloads/responses/messages/MessageResponse.java
+++ product-service/src/main/java/com/alahsantrust/productservice/data/payloads/responses/messages/MessageResponse.java
@@ -5,5 +5,5 @@ public class MessageResponse {
     // youtube link :
     //https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=1
 
-
+// https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=1
 }
