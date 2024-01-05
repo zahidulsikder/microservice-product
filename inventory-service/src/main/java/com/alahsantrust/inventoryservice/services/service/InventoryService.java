77beaba2c6c2db31f443396f53dd79e671e75ebf
zahidulsikder <mdzahidulsikder@gmail.com>
2024-01-05 19:47:46 +0600

    microservice project build and three service running named product service- inventory service- order service.

--- inventory-service/src/main/java/com/alahsantrust/inventoryservice/services/service/InventoryService.java
+++
@@ -1,8 +0,0 @@
-package com.alahsantrust.inventoryservice.services.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface InventoryService {
-    public boolean isInStock(String skuCode);
-}
