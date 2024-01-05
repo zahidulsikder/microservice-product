26ef06b4bf6a2e8d5975e2f45a82ec5d1a41f2ba
zahidulsikder <mdzahidulsikder@gmail.com>
2024-01-05 17:31:51 +0600

    inventory service create class servcie and controller and create one method isInStock(skuCode);

--- inventory-service/src/main/java/com/alahsantrust/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/alahsantrust/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.alahsantrust.inventoryservice;
 
+import com.alahsantrust.inventoryservice.data.models.entity.Inventory;
+import com.alahsantrust.inventoryservice.data.payloads.responses.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +13,26 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
+
+//	@Bean
+//	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
+//		return args -> {
+//			Inventory inventory = new Inventory();
+//			inventory.setSkuCode("iPhone_13_yellow");
+//			inventory.setQuantity(500);
+//			inventoryRepository.save(inventory);
+//			System.out.println(inventory);
+//
+//			Inventory inventory1 = new Inventory();
+//			inventory.setSkuCode("iPhone_13_black");
+//			inventory.setQuantity(0);
+//
+//
+//			inventoryRepository.save(inventory1);
+//			System.out.println(inventory1);
+//		};
+//	}
+
+
 
 }
--- order-service/src/main/java/com/alahsantrust/orderservice/services/OrderService.java
+++ order-service/src/main/java/com/alahsantrust/orderservice/services/OrderService.java
@@ -7,12 +7,14 @@ import com.alahsantrust.orderservice.data.payloads.dto.OrderRequest;
 import com.alahsantrust.orderservice.data.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
