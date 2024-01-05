c0f0c7eb7a3c3397daf650ce0dd2fd4829f92839
zahidulsikder <mdzahidulsikder@gmail.com>
2024-01-05 19:39:13 +0600

    1st build as microservice module(product service, my order service and inventory service)

---
+++ microservice_parent/product-service/src/main/java/com/alahsantrust/productservice/data/payloads/responses/messages/TutorialInfo.java
@@ -0,0 +1,38 @@
+package com.alahsantrust.productservice.data.payloads.responses.messages;
+
+public class TutorialInfo {
+
+    /**
+     * link : https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=1
+     * day : 1
+     * total part : 12
+     * part : 1
+     * date : 25-12-2023
+     * time : 34:31
+     * learning : junit test
+     */
+
+
+    /**
+     * link : https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=1
+     * day : 2
+     * total part : 12
+     * part : 1
+     * date : 27-12-2023
+     * time : 1:05:58
+     * learning : order service
+     * fetcher : create, findAll
+     */
+
+    /**
+     * link : https://www.youtube.com/watch?v=lh1oQHXVSc0&list=PLSVW22jAG8pBnhAdq9S8BpLnZ0_jVBj0c&index=1
+     * day : 3
+     * total part : 12
+     * part : 1
+     * date : 05-01-2024
+     * time : 1:20:00
+     * learning : CommandLineRunner this is automatic create or insert data;
+     * fetcher : isInStock method created
+     */
+
+}
